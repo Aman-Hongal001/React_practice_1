38912ea83ce450bcc9785ce1eb6b1d8e10345716
Aman <amanhongal@gmail.com>
2024-09-22 10:35:18 +0530

    passing data to child in loop
    Place ur content in the new folder called Data in src and make a jsx file to store ur content. After that import that data in your app.js file where u want it to display and use the maping technic suppose blog is the object where data is stored. {blog.map((v,i)=>{return( <Productitems pitem={v} key={i}/> )})}. Pass it to its child/component and use that values in the component.

--- src/App.js
+++ src/App.js
@@ -52,9 +52,127 @@
 
 
 
+// import './App.css';
+// import { Footer } from './Footer';
+// import Header from './Header';
+// // import Container from 'react-bootstrap/Container';
+// // import Row from 'react-bootstrap/Row';
+// // import Col from 'react-bootstrap/Col';
+// // import Card from 'react-bootstrap/Card';
+// // import Button from 'react-bootstrap/Button';
+// import { Button, Row, Col, Card, Container} from 'react-bootstrap';
+
+// function App() {
+
+//   let someInfo = {
+//       "name":"Aman",
+//       "yname":"WSCube"
+//   };
+//   let num = 8924523534
+//   return(
+//     <div>
+//       {/* calling header component */}
+//       {/* passing the values to header using probs */}
+//       <Header info={someInfo} num={num}/>
+
+//       <div className='row'>
+//         <CardDiv/>
+//         <CardDiv/>
+//         <CardDiv/>
+//         <CardDiv/>
+//         <CardDiv/>
+//         <CardDiv/>
+//         <CardDiv/>
+//         <CardDiv/>
+//         <CardDiv/>
+//         <CardDiv/>
+//         <CardDiv/>
+//         <CardDiv/>
+//       </div>
+      
+//       <div className='main'>
+//         <Container fluid>
+//           <Container>
+//             <Row>
+//               <Col lg='3' md='6'>
+//                 <Card style={{ width: '18rem' }}>
+//                   <Card.Img variant="top" src="holder.js/100px180" />
+//                   <Card.Body>
+//                     <Card.Title>Card Title</Card.Title>
+//                     <Card.Text>
+//                       Some quick example text to build on the card title and make up the
+//                       bulk of the card's content.
+//                     </Card.Text>
+//                     <Button variant="primary">Go somewhere</Button>
+//                   </Card.Body>
+//                 </Card>
+//               </Col>
+//               <Col lg='3' md='6'>
+//                 <Card style={{ width: '18rem' }}>
+//                   <Card.Img variant="top" src="holder.js/100px180" />
+//                   <Card.Body>
+//                     <Card.Title>Card Title</Card.Title>
+//                     <Card.Text>
+//                       Some quick example text to build on the card title and make up the
+//                       bulk of the card's content.
+//                     </Card.Text>
+//                     <Button variant="primary">Go somewhere</Button>
+//                   </Card.Body>
+//                 </Card>
+//               </Col>
+//               <Col lg='3' md='6'>
+//                 <Card style={{ width: '18rem' }}>
+//                   <Card.Img variant="top" src="holder.js/100px180" />
+//                   <Card.Body>
+//                     <Card.Title>Card Title</Card.Title>
+//                     <Card.Text>
+//                       Some quick example text to build on the card title and make up the
+//                       bulk of the card's content.
+//                     </Card.Text>
+//                     <Button variant="primary">Go somewhere</Button>
+//                   </Card.Body>
+//                 </Card>
+//               </Col>
+//               <Col lg='3' md='6'>
+//                 <Card style={{ width: '18rem' }}>
+//                   <Card.Img variant="top" src="holder.js/100px180" />
+//                   <Card.Body>
+//                     <Card.Title>Card Title</Card.Title>
+//                     <Card.Text>
+//                       Some quick example text to build on the card title and make up the
+//                       bulk of the card's content.
+//                     </Card.Text>
+//                     <Button variant="primary">Go somewhere</Button>
+//                   </Card.Body>
+//                 </Card>
+//               </Col>
+//             </Row>
+//           </Container>
+//         </Container>
+//       </div>
+
+//       {/* calling footer component */}
+//       <Footer info2={someInfo} num2={num}>
+//         <h1>Welcome to footer section (Inside Footer tag)</h1>
+//       </Footer>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// function CardDiv(){
+//   return(
+//     <div className='cardItem'>
+//       cardDiv
+//     </div>
+//   )
+// }
+
 import './App.css';
 import { Footer } from './Footer';
 import Header from './Header';
+import { blog } from './Data/blog';
 // import Container from 'react-bootstrap/Container';
 // import Row from 'react-bootstrap/Row';
 // import Col from 'react-bootstrap/Col';
@@ -75,82 +193,22 @@ function App() {
       {/* passing the values to header using probs */}
       <Header info={someInfo} num={num}/>
 
-      <div className='row'>
-        <CardDiv/>
-        <CardDiv/>
-        <CardDiv/>
-        <CardDiv/>
-        <CardDiv/>
-        <CardDiv/>
-        <CardDiv/>
-        <CardDiv/>
-        <CardDiv/>
-        <CardDiv/>
-        <CardDiv/>
-        <CardDiv/>
-      </div>
-      
-      <div className='main'>
-        <Container fluid>
-          <Container>
-            <Row>
-              <Col lg='3' md='6'>
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col lg='3' md='6'>
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col lg='3' md='6'>
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col lg='3' md='6'>
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-          </Container>
-        </Container>
-      </div>
 
+      <Container fluid>
+        <Container>
+          <Row>
+            { blog.map((v,i)=>{
+              return(
+                <ProductItem pitem={v} key={i}/>
+              );
+            }) 
+            }
+            
+          </Row>
+        </Container>
+      </Container>
+
+      
       {/* calling footer component */}
       <Footer info2={someInfo} num2={num}>
         <h1>Welcome to footer section (Inside Footer tag)</h1>
@@ -161,10 +219,19 @@ function App() {
 
 export default App;
 
-function CardDiv(){
+function ProductItem({pitem}){
   return(
-    <div className='cardItem'>
-      cardDiv
+    <div className='col-lg-3 mb-4'>
+      
+      <Card>
+        <Card.Body>
+          <Card.Title>{pitem.title}</Card.Title>
+          <Card.Text>
+            {pitem.body}
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
